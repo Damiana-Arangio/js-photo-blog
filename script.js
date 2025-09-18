5b092ae7a8e7844e20898566e8f32e54c890a4ae
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-18 17:50:12 +0200

    Implementazione parziale Milestone 2 (2° parte):
    - Aggiunto overlay al click delle foto
    
    - Manca gestione overlay al click del bottone

--- script.js
+++ script.js
@@ -2,23 +2,40 @@
     DEFINIZIONI
 ******************/
 const enpointPictures = "https://lanciweb.github.io/demo/api/pictures/";                /* Endpoint API */
-const containerSezioneCard = document.getElementById("container-sezione-card");         /* Recupero elementi dal DOM */
+const containerSezioneCard = document.getElementById("container-sezione-card");         /* Recupero contenitore sezione card dal DOM */
 let items = "";                                                                          /* Stringa che conterrà gli elementi da aggiungere al DOM */
+
+
+/*******************
+    CREAZIONE CARD
+*******************/
 
 // Chiamata Ajax con Axios
 axios.get(enpointPictures)
 
     /* Codice da eseguire in caso di successo */
-    .then((rispostaApiSuccess) => {
-        const datiCards = rispostaApiSuccess.data        // Recupero array di oggetti da API
+    .then( (rispostaApiSuccess) => {
+        
+        // Recupero array di oggetti da API
+        const datiCards = rispostaApiSuccess.data
 
         // Ciclo per creare le cards
-        datiCards.forEach(datoCard => {
+        datiCards.forEach( (datoCard) => {
             creaCard(datoCard);
         });
 
-        // Aggiungo elementi al DOM
-        containerSezioneCard.innerHTML = items;                  
+        // Aggiungo card al DOM
+        containerSezioneCard.innerHTML = items;
+        
+        /**************
+            OVERLAY
+        ***************/
+        /* Recupero card dal DOM */
+        const cardCreate = document.querySelectorAll(".card");  
+        
+        /* Chiamata funzione per associare l'evento alle card */
+        associaEventoCard(cardCreate);
+
     })
 
     /* Codice da eseguire in caso di errore */
@@ -26,10 +43,15 @@ axios.get(enpointPictures)
         console.error(rispostaApiError);
     })
 
+
+
 /*****************
     FUNZIONI
 ******************/
+
+// Funzione che riceve i dati di una card e li concatena in una stringa "item"
 function creaCard(datoCard) {
+
     // Inverto stringa data per inserirla nell'attributo date time
     const dataReverse = datoCard.date.split("-").reverse().join("-");
     
@@ -45,4 +67,24 @@ function creaCard(datoCard) {
 
         </article>`
     ;
+}
+
+// Funzione che gestisce l'overlay
+function associaEventoCard(cardCreate) {
+
+    /* Ciclo le card per associare un evento (click) */
+    cardCreate.forEach((cardCreata) => {
+
+        // Associo evento (click) alla card corrente */
+        cardCreata.addEventListener("click", () => {
+        
+            /* Recupero elementi dal DOM per gestire overlay */
+            const overlaySfondo = document.getElementById("overlay-sfondo");
+            const containerOverlayContent = document.getElementById("container-overlay-content");
+
+            /* Rimuovo classe display-none per gestire l' overlay */
+            overlaySfondo.classList.remove("display-none");
+            containerOverlayContent.classList.remove("display-none");
+        })
+    });
 }
