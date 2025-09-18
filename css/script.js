b7d9b3ada3ceb1761a56c1b6480beac906b0d81e
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-18 02:03:34 +0200

    Implementazione Milestone 2 completata:
    - Testata API che restituisce un array di 6 oggetti, ognuno contenente 4 proprietà: id, title, date, url

---
+++ css/script.js
@@ -0,0 +1,12 @@
+/*****************
+    DEFINIZIONI
+******************/
+
+/* Endpoint API */
+const enpointPictures = "https://lanciweb.github.io/demo/api/pictures/";
+
+/* 
+    Testata API su Postman: 
+    restituisce un array di 6 oggetti, 
+    ognuno contenente 4 proprietà: id, title, date, url
+*/
